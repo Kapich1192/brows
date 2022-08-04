b3570e8841ee95869ef687a04b47dcbcd15ee44f
kapich1192 <kapich1192@yandex.ru>
2022-08-04 13:27:56 +0300

    dirst commit

---
+++ src/main/java/com/example/brows/BrowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.brows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brows/BrowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
